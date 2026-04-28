5ac29af1b7346693382164c58a5865018d781079
C-S26 <pavanprakash0909@gmail.com>
2026-04-28 17:24:17 +0530

    retail logger updated

--- logger.py
+++ logger.py
@@ -1,12 +1,22 @@
 import csv
 from datetime import datetime
+import os
 
 def log(payload, result):
-    with open("results.csv", "a", newline="") as file:
-        writer =  csv.writer(file)
+    file_path = "results.csv"
+
+    file_exists = os.path.isfile(file_path)
+
+    with open(file_path, "a", newline="") as file:
+        writer = csv.writer(file)
+
+        # Write header only once
+        if not file_exists:
+            writer.writerow(["Timestamp", "Payload", "Result", "Severity"])
+
         writer.writerow([
-         datetime.now(),
-         payload,
-         result,
-         "HIGH" if "Flaw" in result or "Crash" in result else "LOW"
+            datetime.now(),
+            payload,
+            result,
+            "HIGH" if any(x in result for x in ["Flaw", "Detected", "IDOR", "Crash"]) else "LOW"
         ])
